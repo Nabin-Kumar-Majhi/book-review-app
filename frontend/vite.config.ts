ca8d5cea3b61d0b3d9646a44726bb92d7f4b4422
Nabin Kumar Majhi <naveenluffy2060@gmail.com>
2024-11-20 08:10:28 +0545

    Deployment push

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,14 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  envDir: "./",  // .env file ko location
+  server: {
+    port: 5173,
+  },
+  define: {
+    'process.env.VITE_BACKEND_URL': JSON.stringify(process.env.VITE_BACKEND_URL)
+  }
 })
